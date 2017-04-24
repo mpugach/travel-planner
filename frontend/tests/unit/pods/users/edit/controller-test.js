3a4189cdbf09d922e7cc0ad61d9649b2b240ea2e
Maksym Pugach <pugach.m@gmail.com>
2017-04-24 22:45:10 +0300

    fix frontend tests

--- frontend/tests/unit/pods/users/edit/controller-test.js
+++ frontend/tests/unit/pods/users/edit/controller-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('controller:users/edit', 'Unit | Controller | users/edit', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['service:currentUser']
 });
 
 // Replace this with your real tests.
