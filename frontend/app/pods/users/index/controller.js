45cf4e6163cd6befaca9da32aec3f06c172497fd
Maksym Pugach <pugach.m@gmail.com>
2017-04-25 22:01:01 +0300

    add confirmation modal on user delete

--- frontend/app/pods/users/index/controller.js
+++ frontend/app/pods/users/index/controller.js
@@ -1,20 +1,35 @@
 import Ember from 'ember';
 
 const {
+  get,
+  set,
   Controller,
+  setProperties,
 } = Ember;
 
 export default Controller.extend({
+  showDeleteConfirmation: false,
+
   actions: {
     goToUser(user) {
       this.transitionToRoute('users.edit', user);
     },
 
-    destroyUser(user) {
-      user
-        .destroyRecord()
-        .then(() => this.toast.success('User is deleted!'))
-        .catch(() => this.toast.error('Something went wrong...'));
+    destroyUser(userToDelete) {
+      setProperties(this, { userToDelete, showDeleteConfirmation: true });
+    },
+
+    closeDeleteConfirmation(result) {
+      if (result === 'yes') {
+        const user = get(this, 'userToDelete');
+
+        user
+          .destroyRecord()
+          .then(() => this.toast.success(`User ${get(user, 'email')} is deleted!`))
+          .catch(() => this.toast.error('Something went wrong...'));
+      }
+
+      set(this, 'showDeleteConfirmation', false);
     },
   },
 });
