e31e5566d33bf856fab5e6c4df360986fd1d8602
Maksym Pugach <pugach.m@gmail.com>
2017-05-01 14:33:00 +0300

    remove unneeded service injection

--- frontend/app/pods/components/trip-form/component.js
+++ frontend/app/pods/components/trip-form/component.js
@@ -2,13 +2,8 @@ import Ember from 'ember';
 
 const {
   Component,
-  inject: {
-    service,
-  },
 } = Ember;
 
 export default Component.extend({
   tagName: '',
-
-  currentUser: service(),
 });
