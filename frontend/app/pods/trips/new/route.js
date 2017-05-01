f89a8000c8f0eb3a6fd7de70dc1bbccb431074ca
Maksym Pugach <pugach.m@gmail.com>
2017-05-02 01:03:13 +0300

    do not allow unauthenticated user to navigate trip routes

--- frontend/app/pods/trips/new/route.js
+++ frontend/app/pods/trips/new/route.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 const {
   get,
   Route,
 } = Ember;
 
-export default Route.extend({
+export default Route.extend(AuthenticatedRouteMixin, {
   model() {
     const today = new Date;
 
